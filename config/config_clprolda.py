080fb6d478894c2ae60710b3de805c678b3be1f2
Duong Nguyen <duong05102002@gmail.com>
2023-02-11 15:24:14 +0700

    add some configs

--- config/config_clprolda.py
+++ config/config_clprolda.py
@@ -1,8 +1,18 @@
-class Config:
-    hidden_size = 200
-    num_topics = 100
+import torch
+import pickle
+
+with open('./data/bow.pkl', 'rb') as f:
+    bow = pickle.load(f)
+
+class Config():
+    hidden_size = 100
+    num_topics = 20
     drop_lda = 0.2
-    vocab_size = 30000
-    k = 100
-    learn_prior = False
-    
+    vocab_size = bow.shape[1]
+    k = 50
+    learn_prior = False # nếu train thì loss hay ra nan  
+    device = 'cuda' if torch.cuda.is_available() else 'cpu' 
+    lr = 1e-4
+    train_cl = True # dùng constractive hay không dùng thì kết quả vẫn chán na ná nhau (khả năng prodlda hoạt động kém)
+    batch_size = 16
+    epochs = 10
